a5044c9d8f9b17d8070d92e0d5e83d5105784311
MrR341-GER <55445114+MrR341-GER@users.noreply.github.com>
2023-10-29 07:51:50 +0100

    Update ShutUp-EpicLauncherAchievements.bat

--- ShutUp-EpicLauncherAchievements.bat
+++ ShutUp-EpicLauncherAchievements.bat
@@ -3,14 +3,6 @@
 set process_name=EOSOverlayRenderer-Win64-Shipping.exe
 
 :loop
-tasklist | findstr /i %process_name% >nul
-if not errorlevel 1 (
-    taskkill /f /im %process_name%
-    if errorlevel 1 (
-        echo Failed to terminate the process.
-    ) else (
-        echo Process terminated.
-    )
-)
+taskkill /f /im %process_name% >nul 2>&1
 timeout /nobreak /t 1 >nul
 goto loop
